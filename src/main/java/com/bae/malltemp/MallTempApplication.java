f0f17fdca5e3ab0fca3d60b8030f93e760d0a79f
baedj <qoehdwns>
2023-10-31 09:36:48 +0900

    첫 프로젝트

---
+++ src/main/java/com/bae/malltemp/MallTempApplication.java
@@ -0,0 +1,13 @@
+package com.bae.malltemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTempApplication.class, args);
+    }
+
+}
